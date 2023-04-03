8bc60c16e1d7bd39f06986bc85b2f88b5b4a3ff3
Ghayth BenGara <ghayth.bengara.26@gmail.com>
2023-04-03 17:57:30 +0200

    Terminer la premiére partie de l'affichage de la liste des pdf

--- app/Http/Livewire/ListePfesLivewire.php
+++ app/Http/Livewire/ListePfesLivewire.php
@@ -1,13 +1,54 @@
 <?php
+    namespace App\Http\Livewire;
+    use Livewire\Component;
+    use Livewire\WithPagination;
+    use Illuminate\Pagination\Paginator;
+    use App\Models\AnneeUniversitaire;
+    use App\Models\Pfe;
 
-namespace App\Http\Livewire;
+    class ListePfesLivewire extends Component{
+        public $annee_universitaire = "Tout";
+        public $search_pfe;
+        public $currentPage = 1;
+        use WithPagination;
 
-use Livewire\Component;
+        public function render(){
+            if($this->annee_universitaire == "Tout"){
+                return view('livewire.liste-pfes-livewire', [
+                    'pfes' => Pfe::where([
+                        ["pfes.titre", "like", "%".$this->search_pfe."%"],
+                    ])
 
-class ListePfesLivewire extends Component
-{
-    public function render()
-    {
-        return view('livewire.liste-pfes-livewire');
+                    ->join("annees_universitaires", "annees_universitaires.id_annee_universitaire", "=", "pfes.id_annee_universitaire")
+                    ->orderBy("pfes.date_creation_pdf", "asc")
+                    ->paginate(10, array("pfes.*", "annees_universitaires.*"))
+                ]);
+            }
+
+            else{
+                return view('livewire.liste-pfes-livewire', [
+                    "pfes" => Pfe::where([
+                        ["pfes.titre", "like", "%".$this->search_pfe."%"],
+                        ["annees_universitaires.id_annee_universitaire", "like", "%".$this->annee_universitaire."%"],
+                    ])
+
+                    ->join("annees_universitaires", "annees_universitaires.id_annee_universitaire", "=", "pfes.id_annee_universitaire")
+                    ->orderBy("pfes.date_creation_pdf", "asc")
+                    ->paginate(10, array("pfes.*", "annees_universitaires.*"))
+                ]);
+            }
+        }
+
+        public function getListeAnneesUniversitaires(){
+            return AnneeUniversitaire::orderBy("debut", "asc")->get();
+        }
+
+        public function setPage($url){
+            $this->currentPage = explode("page=", $url)[1];
+
+            Paginator::currentPageResolver(function(){
+                return $this->currentPage;
+            });
+        }
     }
-}
+?>
--- resources/views/livewire/liste-pfes-livewire.blade.php
+++ resources/views/livewire/liste-pfes-livewire.blade.php
@@ -1,12 +1,117 @@
 <div>
-    <div class = "row mb-2">
-        @if(auth()->user()->getRoleUserAttribute() == "Bibliothécaire")
+    @if(auth()->user()->getRoleUserAttribute() == "Bibliothécaire")
+        <div class = "row mb-2">
             <div class = "col-md-8">
                 <a href = "{{url('/add-pfe')}}" class = "btn btn-primary mb-2">
                     <i class = "mdi mdi-plus-circle me-2"></i> 
                     Créer un projet de fin d'étude
                 </a>
             </div>
-        @endif
+            <div class = "col-md-4">
+                <select class = "form-select" id = "annee_universitaire" name = "annee_universitaire" wire:model = "annee_universitaire" required>
+                    <option value = "Tout" selected disabled>Sélectionnez l'année universitaire..</option>
+                    @if(count($this->getListeAnneesUniversitaires()) == 0)
+                            <option value = "#" selected disabled>La liste des années universitaires est vide.</option>
+                        @else
+                            @foreach($this->getListeAnneesUniversitaires() as $data)
+                                <option value = "{{$data->id_annee_universitaire}}">Année universitaire {{$data->debut}} - {{$data->fin}}</option>
+                            @endforeach
+                        @endif
+                </select>
+            </div>
+        </div>
+        <div class = "row mb-2">
+            <div class = "col-md-12">
+                <div class = "mb-3">
+                    <input type = "text" class = "form-control" id = "search_pfe" name = "search_pfe" placeholder = "Chercher un pfe.." wire:model = "search_pfe" required>
+                </div>
+            </div>
+        </div>
+    @endif
+    <div class = "row">
+        <div class = "col-12">
+            <div class = "board">
+                <div class = "tasks" data-plugin = "dragula">
+                    <h5 class = "mt-0 task-header">Projets de fin d'étude ({{count($pfes)}})</h5>
+                    <div id = "task-list-one" class = "row task-list-items p-2">
+                        @if(!empty($pfes) && ($pfes->count()))
+                            @foreach($pfes as $data)
+                                <div class = " col-md-5 card mb-0 mx-auto">
+                                    <div class = "card-body p-3">
+                                        <small class = "float-end text-muted text-capitalize mt-1">
+                                            <?php
+                                                setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+                                                echo utf8_encode($data->getFormattedDateCreationPdfAttribute())
+                                            ?>
+                                        </small>
+                                        @if($data->debut < date("Y") && $data->fin < date("Y"))
+                                            <span class = "badge bg-danger p-2">Année universitaire dépassée</span>
+                                        @elseif($data->debut < date("Y") && $data->fin == date("Y"))
+                                            <span class = "badge bg-danger p-2">Année universitaire dépassée</span>
+                                        @elseif($data->debut == date("Y") && $data->fin > date("Y"))
+                                            <span class = "badge bg-success p-2">Année universitaire actuelle</span>
+                                        @else
+                                            <span class = "badge bg-secondary p-2">Année universitaire à venir</span>
+                                        @endif
+                                        <h5 class = "mt-2 mb-2">
+                                            <a href = "javascript:void(0)" class = "text-body">{{$data->getTitreAttribute()}}</a>
+                                        </h5>
+                                        <p class = "mb-0">
+                                            <span class = "pe-2 text-nowrap mb-2 d-inline-block">
+                                                <i class = "mdi mdi-calendar-minus text-muted"></i>
+                                                {{$data->debut}}
+                                            </span>
+                                            <span class = "pe-2 text-nowrap mb-2 d-inline-block">
+                                                <i class = "mdi mdi-calendar-plus text-muted"></i>
+                                                {{$data->fin}}
+                                            </span>
+                                        </p>
+                                        <div class = "dropdown float-end">
+                                            <a href = "javascript:void(0)" class = "dropdown-toggle text-muted arrow-none" data-bs-toggle = "dropdown" aria-expanded = "false">
+                                                <i class = "mdi mdi-dots-vertical font-18"></i>
+                                            </a>
+                                            <div class = "dropdown-menu dropdown-menu-end">
+                                                <a href = "" class = "dropdown-item">
+                                                    <i class = "mdi mdi-eye me-1"></i>
+                                                    Consulter
+                                                </a>
+                                                @if(auth()->user()->getRoleUserAttribute() == "Bibliothécaire")
+                                                    <a href = "" class = "dropdown-item">
+                                                        <i class = "mdi mdi-pencil me-1"></i>
+                                                        Modifier
+                                                    </a>
+                                                    <a href = "" class = "dropdown-item">
+                                                        <i class = "mdi mdi-pencil me-1"></i>
+                                                        Supprimer
+                                                    </a>
+                                                    <a href = "" class = "dropdown-item">
+                                                        <i class = "mdi mdi-download me-1"></i>
+                                                        Télécharger
+                                                    </a>
+                                                @elseif(auth()->user()->getRoleUserAttribute() == "Enseignant")
+                                                    <a href = "" class = "dropdown-item">
+                                                        <i class = "mdi mdi-download me-1"></i>
+                                                        Télécharger
+                                                    </a>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <p class = "mb-0">
+                                            <img src = "{{URL::asset('/images/pdf.png')}}" alt = "Image de pdf" class = "avatar-xs rounded-circle me-1">
+                                            <span class = "align-middle">Fichier PDF</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <p class = "text-center h5 mt-4">Aucun pfe actuellement trouvé.</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "mt-3 mb-3">
+        {{$pfes->links("vendor.pagination.pagination_livewire")}}
     </div>
 </div>
